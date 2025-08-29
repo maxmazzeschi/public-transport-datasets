9701dfb0b13c5b6d1449e570f0bd745a3637f62b
max.mazzeschi <max.mazzeschi@live.com>
2025-08-29 14:38:20 +0200

    .

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="public_transport_datasets",
-    version="0.1.59",
+    version="0.1.58",
     packages=find_packages(),
     install_requires=[
         "requests",
